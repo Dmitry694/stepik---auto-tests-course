e2988f2d8c7cde964f2f3bed2854b0bef4d4432b
Dmitry M <dmitry.m694@gmail.com>
2020-12-26 15:12:41 +0200

    Create L3_3_P3_output_PyTest.py
    Third Parts

---
+++ Tests_framework/L3_3_P3_output_PyTest.py
@@ -0,0 +1,65 @@
+import unittest
+from selenium import webdriver
+import time
+
+link1 = "http://suninjuly.github.io/registration1.html"
+link2 = "http://suninjuly.github.io/registration2.html"
+lns = (link1, link2)
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def test_registration1(self):
+
+        self.browser.get(link1)
+
+        each_fn = self.browser.find_element_by_css_selector(
+                "body > div > form > div.first_block > div.form-group.first_class > input").send_keys("Ivan")
+        #try:
+        search_ln = self.browser.find_element_by_css_selector(
+                "body > div > form > div.first_block > div.form-group.second_class > input").send_keys("Dorn")
+        #except Exception:
+        #    pass
+        search_em = self.browser.find_element_by_css_selector(
+        "body > div > form > div.first_block > div.form-group.third_class > input").send_keys("123@123.ru")
+        button = self.browser.find_element_by_css_selector("button.btn").click()
+
+        welcome_text_elt = self.browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", "test down =( {}".format(link1))
+
+    def test_registration2(self):
+        self.browser.get(link2)
+
+        each_fn = self.browser.find_element_by_css_selector(
+            "body > div > form > div.first_block > div.form-group.first_class > input").send_keys("Ivan")
+        # try:
+        search_ln = self.browser.find_element_by_css_selector(
+            "body > div > form > div.first_block > div.form-group.second_class > input").send_keys("Dorn")
+        # except Exception:
+        #    pass
+        search_em = self.browser.find_element_by_css_selector(
+            "body > div > form > div.first_block > div.form-group.third_class > input").send_keys("123@123.ru")
+        button = self.browser.find_element_by_css_selector("button.btn").click()
+
+        welcome_text_elt = self.browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
+                         "test down =( {}".format(link2))
+
+
+    def tearDown(self):
+        time.sleep(2)
+        self.browser.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
